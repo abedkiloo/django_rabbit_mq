2f0f6f5b234808f36b98e511ba92969f94d50879
ABEDNEGO.WAMBUA@equitybank.co.ke <ABEDNEGO.WAMBUA@equitybank.co.ke>
2024-01-23 01:48:20 +0300

    - Added url to consume xml upload APiVIEW - Added class to get xml from API and extra the element tree data

--- django_rabbit_mq/ProductService/products/urls.py
+++ django_rabbit_mq/ProductService/products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from products.views import ProductCategoryViewSet, ProductViewSet
+from products.views import ProductCategoryViewSet, ProductViewSet, ImportProducts
 
 app_name = "products"
 
@@ -9,7 +9,7 @@ router = DefaultRouter()
 router.register(r"categories", ProductCategoryViewSet)
 router.register(r"", ProductViewSet)
 
-
 urlpatterns = [
+    path(r'import-xml/', ImportProducts.as_view(), name='import_xml_api'),
     path("", include(router.urls)),
 ]
--- django_rabbit_mq/ProductService/products/views.py
+++ django_rabbit_mq/ProductService/products/views.py
@@ -1,5 +1,10 @@
 from rest_framework import permissions, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from xml.etree import ElementTree as ET
+from rest_framework import status
 
+from rest_framework.parsers import MultiPartParser
 from products.models import Product, ProductCategory
 from products.permissions import IsSellerOrAdmin
 from products.serializers import (
@@ -41,3 +46,37 @@ class ProductViewSet(viewsets.ModelViewSet):
             self.permission_classes = (permissions.AllowAny,)
 
         return super().get_permissions()
+
+
+class ImportProducts(APIView):
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            uploaded_file = request.FILES.get("products")
+            if not uploaded_file:
+                return Response({"error": "XML File was not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+            tree = ET.parse(uploaded_file)
+            root = tree.getroot()
+
+
+            imported_data = []
+            for item in root.findall('product'):
+                name, desc, quantity, price = item.find('name').text, item.find('desc').text, item.find(
+                    'quantity').text, item.find('price').text
+                imported_data.append([name, desc, quantity, price])
+            return Response(imported_data, status=status.HTTP_200_OK)
+        except ET.ParseError:
+            return Response({"error": "Invalid XML"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": f"An error {str(e)} occurred"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def read_xm_products(xml_file_path):
+    try:
+        with open(xml_file_path, 'r') as xml_file:
+            xml_data = xml_file.read()
+        headers = {"Content-Type": 'application/xml'}
+    except FileNotFoundError:
+        print("File Not Found")
